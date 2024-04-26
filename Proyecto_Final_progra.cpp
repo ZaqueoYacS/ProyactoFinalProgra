c6d36897f21dcfdb52cb204488b6bf2b51d45e6e
ZaqueoYacS <brandonzys5120@umes.edu.gt>
2024-04-25 19:39:58 -0600

    Primer menu

--- Proyecto_Final_progra.cpp
+++ Proyecto_Final_progra.cpp
@@ -3,10 +3,46 @@
  using namespace std;
 
  int main(){
-    cout << "prueba del primer commit" << endl;
-    cout << "segunda prueba de commit marco " << endl;
-    cout << "tercera prueba de commit " << endl;
-    return 0;
+int repetirP;
+int qJugador;
+string nJ1, nJ2;
+
+do{ 
+   cout << " ¿quieres jugar con alguien más?------presiona (1) \n " << "¿quires jugar con el computador?------presiona(2)" << endl;
+   cin >> qJugador;
+
+   switch (qJugador)
+   {
+   case 1:
+
+   cout << "Ingrese el nombre del jugador 1: ";
+   cin >> nJ1;
+   cout << " ";
+   cout << "Ingrese el nombre del Jugadro 2: ";
+   cin >> nJ2;
+
+   break;
+
+   case 2:
+   cout << "Ingrese el nombre del jugador 1: ";
+   cin >> nJ1;
+
+   break;
+   
+   default:
+   cout << "el numero no corresponde a ninguna opción" << endl;
+      break;
+   }
+
+
+   cout << "¿quieres repetir el programa? \n" << " 1 = si \n" << " 2 = no" << endl;
+   cin >> repetirP;
+
+ }while(repetirP == 1);
+
+ cout << "gracis por jugar :)" << endl;
+
+  return 0;
  }
 
 
