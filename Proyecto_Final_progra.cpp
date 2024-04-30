1a3e470994d0b2cb75ff14f8267d88bf6699ef77
ZaqueoYacS <brandonzys5120@umes.edu.gt>
2024-04-29 19:21:30 -0600

    avances

--- Proyecto_Final_progra.cpp
+++ Proyecto_Final_progra.cpp
@@ -1,37 +1,36 @@
 #include <iostream>
+#include <string>
 
  using namespace std;
 
+const string ncomp = "computador";
+ int nJugadores;
  string nJ1, nJ2;
+ char J1, J2, comp;
 
 int preguntarCantidadJugadores();
  string solicitarNombre(int jNum);
+ void elegirSimbolo(char &J1, char &J2);
+
 
  int main(){
- int repetirP;
 
-do{ 
-   int nJugadores = preguntarCantidadJugadores();
+ nJugadores = preguntarCantidadJugadores();
 
     switch (nJugadores) {
         case 1:
             nJ1 = solicitarNombre(1);
+            elegirSimbolo(J1, J2);
             break;
         case 2:
             nJ1 = solicitarNombre(1);
             nJ2 = solicitarNombre(2);
+            elegirSimbolo(J1, J2);
             break;
         default:
             cout << "numero de jugadores no valido." << endl;
             return 1;
     }
-
-   cout << "¿quieres repetir el juego? \n" << " 1 = si \n" << " presiona cualquier tecla diferente de 1 para salir" << endl;
-   cin >> repetirP;
-
- }while(repetirP == 1);
-
- cout << "gracis por jugar :)" << endl;
 
   return 0;
  }
@@ -39,7 +38,7 @@ do{
  int preguntarCantidadJugadores() {
     int nJugadores;
     do {
-        cout <<  " ¿quieres jugar con alguien más?------presiona (2) \n " << "¿quires jugar con el computador?------presiona(1)" << endl;
+        cout <<  " ¿quieres jugar con alguien mas?------presiona (2) \n " << "¿quires jugar con el computador?------presiona(1)" << endl;
         cin >> nJugadores;
     } while (nJugadores != 1 && nJugadores != 2);
 
@@ -52,6 +51,41 @@ string solicitarNombre(int jNum) {
     cin >> nombre;
     return nombre;
 }
+
+void elegirSimbolo(char &J1, char &J2) {
+    if(nJugadores == 2){
+    cout << nJ1<< ", elija su simbolo---------------(X o O): ";
+    cin >> J1;
+    
+    while (J1 != 'X' && J1 != 'x' && J1 != 'O' && J1 != 'o') {
+        cout << "Solamente se pueden elegir-------------X o O------------------: ";
+        cin >> J1;
+    }
+    if (J1 == 'x' || J1 == 'o') {
+        J1 = (J1 == 'x') ? 'X' : 'O';
+    }
+    J2 = (J1 == 'X') ? 'O' : 'X';
+    
+    cout << nJ1 << "----------------: " << J1 << endl;
+    cout << nJ2 << "----------------: " << J2 << endl;
+    }else if(nJugadores == 1){
+        cout << nJ1<< ", elija su simbolo---------------(X o O): ";
+    cin >> J1;
+    
+    while (J1 != 'X' && J1 != 'x' && J1 != 'O' && J1 != 'o') {
+        cout << "Solamente se pueden elegir-------------X o O------------------: ";
+        cin >> J1;
+    }
+    if (J1 == 'x' || J1 == 'o') {
+        J1 = (J1 == 'x') ? 'X' : 'O';
+    }
+    J2 = (J1 == 'X') ? 'O' : 'X';
+    
+    cout << nJ1 << "----------------: " << J1 << endl;
+    cout << ncomp << "----------------: " << J2 << endl;
+    }
+}
+
 
 
  
